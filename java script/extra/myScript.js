bd52cf94ce951cf217404d19c3f5ec87e0cbc9f4
WebDesignTops <119042142+WebDesignTops@users.noreply.github.com>
2022-11-28 11:32:47 +0530

    Add files via upload

---
+++ java script/extra/myScript.js
@@ -0,0 +1,9 @@
+var a,b,c;
+a="Tops";
+b="Technology";
+
+c=a+b;
+
+document.write(c);
+console.log(c)
+alert(c)
---
+++ java script/lecture-3/myScript.js
@@ -0,0 +1,5 @@
+document.write("this is External File")
+
+console.log("this is External File")
+
+alert("this is External File")
